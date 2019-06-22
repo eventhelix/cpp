7f3b48bc4d81a5049a6f781a37290aac00ecb8fa
EventHelix.com Inc <support@eventhelix.com>
2019-06-22 13:35:05 -0400

    improved formatting

--- lambda-expressions/lambda-expression-value-capture.cpp
+++ lambda-expressions/lambda-expression-value-capture.cpp
@@ -5,38 +5,38 @@
 
 // Simulate a long computation
 int long_computation(int x, int duration_seconds,
-	const char identifier)
+                     const char identifier)
 {
-	// Print a character to mark the progress of the computation.
-	for (auto i = 0; i < duration_seconds * 10; ++i) 
-	{
-		std::this_thread::sleep_for(std::chrono::milliseconds{ 100 });
-		printf("%c", identifier);
-	}
-	return 42 + x;
+  // Print a character to mark the progress of the computation.
+  for (auto i = 0; i < duration_seconds * 10; ++i)
+  {
+    std::this_thread::sleep_for(std::chrono::milliseconds{100});
+    printf("%c", identifier);
+  }
+  return 42 + x;
 }
 int main()
 {
-	auto x = 5, y = 6;
-	printf("main thread   : x = %d, y = %d\n", x, y);
-	printf("main thread   : launching lambda thread\n");
+  auto x = 5, y = 6;
+  printf("main thread   : x = %d, y = %d\n", x, y);
+  printf("main thread   : launching lambda thread\n");
 
-	// Execute the lambda function in a separate thread. 
-	// The function returns a future.
-	auto result_future = std::async(std::launch::async, [=]() {
+  // Execute the lambda function in a separate thread.
+  // The function returns a future.
+  auto result_future = std::async(std::launch::async, [=]() {
 		printf("lambda thread : x = %d\n", x);
 		auto answer = long_computation(x, 6, 'l');
 		printf("\nlambda thread : long computation completed\n");
 		return answer; });
 
-	// At this point main thread and lambda thread 
-	// are executing in parallel.
-	x = 0;
-	printf("main thread   : x = %d, y = %d\n", x, y);
-	auto main_thread_result = long_computation(x, 3, 'm');
-	printf("\nmain thread   : long computation completed\n");
-	// Wait for the result from the lambda thread
-	auto lambda_thread_result = result_future.get();
-	printf("main thread   : result = %d\n", main_thread_result);
-	printf("lambda thread : result = %d\n", lambda_thread_result);
+  // At this point main thread and lambda thread
+  // are executing in parallel.
+  x = 0;
+  printf("main thread   : x = %d, y = %d\n", x, y);
+  auto main_thread_result = long_computation(x, 3, 'm');
+  printf("\nmain thread   : long computation completed\n");
+  // Wait for the result from the lambda thread
+  auto lambda_thread_result = result_future.get();
+  printf("main thread   : result = %d\n", main_thread_result);
+  printf("lambda thread : result = %d\n", lambda_thread_result);
 }
--- lambda-expressions/simple-lambda-expression.cpp
+++ lambda-expressions/simple-lambda-expression.cpp
@@ -5,20 +5,20 @@
 // visitor for each item.
 
 template <typename T, typename F>
-void visit(std::vector<T>& vec, const F& visitor) 
+void visit(std::vector<T> &vec, const F &visitor)
 {
-	for (auto item : vec) 
-	{
-		visitor(item);
-	}
+  for (auto item : vec)
+  {
+    visitor(item);
+  }
 }
 
-int main() 
+int main()
 {
-	std::vector<int> vec = { 0, 1, 2 };
+  std::vector<int> vec = {0, 1, 2};
 
-	// Calling visit to print each element of a vector.
-	// The printing code is passed as a lambda function.
-	visit(vec, [](int a) { printf("%d\n", a); });
-	return 0;
+  // Calling visit to print each element of a vector.
+  // The printing code is passed as a lambda function.
+  visit(vec, [](int a) { printf("%d\n", a); });
+  return 0;
 }

b05ba249b7d0ecc86f9a43d5113453914e154894
EventHelix.com Inc <support@eventhelix.com>
2019-06-21 20:56:01 -0400

    Added lambda expression with value based capture

--- lambda-expressions/a-simple-lambda-expression.cpp
+++ lambda-expressions/a-simple-lambda-expression.cpp
@@ -1,13 +1,24 @@
 #include <vector>
 #include <cstdio>
+
+// Visits all elements of a vector vec and invokes the
+// visitor for each item.
+
 template <typename T, typename F>
-void visit(std::vector<T>& vec, const F& visitor) {
-	for (auto item : vec) {
+void visit(std::vector<T>& vec, const F& visitor) 
+{
+	for (auto item : vec) 
+	{
 		visitor(item);
 	}
 }
-int main() {
+
+int main() 
+{
 	std::vector<int> vec = { 0, 1, 2 };
+
+	// Calling visit to print each element of a vector.
+	// The printing code is passed as a lambda function.
 	visit(vec, [](int a) { printf("%d\n", a); });
 	return 0;
 }
